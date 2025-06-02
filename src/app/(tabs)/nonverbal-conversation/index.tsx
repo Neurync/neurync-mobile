cbc1c5919a31cd7f7f49be75d598dbbd912fcec9
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-06-02 12:58:47 -0300

    feat: adicionando linkagem

--- src/app/(tabs)/nonverbal-conversation/index.tsx
+++ src/app/(tabs)/nonverbal-conversation/index.tsx
@@ -3,10 +3,11 @@ import { ButtonIcon } from '@/components/button-icon';
 import { CircleSlash2, HeartCrack, Star } from 'lucide-react-native';
 import { Text, View } from 'react-native';
 import { screenStyle } from '../screen-style';
-import { colors } from '@/constants/colors';
 import { Logo } from '@/components/logo';
+import { useRouter } from 'expo-router';
 
 export default function NonverbalConversation() {
+	const router = useRouter();
 	return (
 		<View style={screenStyle.container}>
 			<Logo />
@@ -17,7 +18,11 @@ export default function NonverbalConversation() {
 					gap: 15,
 				}}
 			>
-				<ButtonIcon icon={HeartCrack} text={'Sentimentos'} />
+				<ButtonIcon
+					onPress={() => router.push('/feelings')}
+					icon={HeartCrack}
+					text={'Sentimentos'}
+				/>
 				<ButtonIcon icon={CircleSlash2} text={'Problemas e\nNecessidades'} />
 				<ButtonIcon icon={Star} text={'Favoritos'} />
 			</View>
---
+++ src/app/feelings/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export default function Feelings() {
+	return <Text>Sentimentos</Text>;
+}
