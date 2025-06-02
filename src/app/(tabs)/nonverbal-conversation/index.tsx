1526c77924100b463e82e1c5f0ec15bcf26d66bb
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-06-02 11:36:00 -0300

    feat: criando a tela de navegação da comunicação não verbal

--- src/app/(tabs)/nonverbal-conversation/index.tsx
+++ src/app/(tabs)/nonverbal-conversation/index.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native';
+import { AlertButton } from '@/components/alert-button';
+import { ButtonIcon } from '@/components/button-icon';
+import { CircleSlash2, HeartCrack, Star } from 'lucide-react-native';
+import { Text, View } from 'react-native';
+import { screenStyle } from '../screen-style';
+import { colors } from '@/constants/colors';
 
 export default function NonverbalConversation() {
 	return (
-		<View>
-			<Text>Tela de comunicação não verbal</Text>
+		<View style={screenStyle.container}>
+			<Text
+				style={{
+					fontWeight: 500,
+					textTransform: 'uppercase',
+					fontSize: 30,
+					color: colors.black,
+				}}
+			>
+				Selecione
+			</Text>
+			<View
+				style={{
+					marginTop: 15,
+					gap: 15,
+				}}
+			>
+				<ButtonIcon icon={HeartCrack} text={'Sentimentos'} />
+				<ButtonIcon icon={CircleSlash2} text={'Problemas e\nNecessidades'} />
+				<ButtonIcon icon={Star} text={'Favoritos'} />
+			</View>
+			<AlertButton />{' '}
 		</View>
 	);
 }
