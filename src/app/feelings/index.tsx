4abe80f858e66a800dfb14791641ae8da2496917
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-06-05 13:00:25 -0300

    feat: adicionando a tela de sentimentos

--- src/app/feelings/index.tsx
+++ src/app/feelings/index.tsx
@@ -1,5 +1,62 @@
-import { Text } from 'react-native';
+import { colors } from '@/constants/colors';
+import { Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Undo2 } from 'lucide-react-native';
+import { screenStyle } from '@/constants/screen-style';
 
 export default function Feelings() {
-	return <Text>Sentimentos</Text>;
+	return (
+		<View
+			style={{
+				backgroundColor: colors.white,
+				height: '100%',
+				width: '100%',
+				...screenStyle,
+			}}
+		>
+			<View
+				style={{
+					width: '100%',
+					height: '12%',
+					backgroundColor: colors.seaGreen,
+					paddingLeft: 5,
+					paddingRight: 5,
+					paddingTop: 20,
+					flexDirection: 'row',
+					alignItems: 'center',
+					justifyContent: 'space-around',
+				}}
+			>
+				<TouchableOpacity onPress={() => console.log('foda-se')}>
+					<Undo2 color={colors.white} size={25} />
+				</TouchableOpacity>
+				<TextInput
+					style={{
+						backgroundColor: colors.lightGray,
+						width: '85%',
+						borderRadius: 25,
+						fontWeight: 500,
+						paddingLeft: 5,
+						fontSize: 16,
+					}}
+					placeholder="Pesquisar..."
+				/>
+			</View>
+
+			<View style={{ width: '100%', paddingTop: 10 }}>
+				<Text style={screenStyle.title}>Eu me sinto...</Text>
+
+				{/* TODO: Fazer listagem de botões de sentimentos */}
+			</View>
+
+			<View
+				style={{
+					backgroundColor: colors.seaGreen,
+					width: '100%',
+					height: '5%',
+					position: 'absolute',
+					bottom: 0,
+				}}
+			/>
+		</View>
+	);
 }
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,9 +2,10 @@ export const colors = {
   black: "#0A0A0A",
   gold: "#EFB41F",
   gray: "#666666",
+  lightGray: "#D9E7EC",
   lightSeaGreen: "#8EE7D1",
   seaGreen: "#48CDAC",
   white: "#FCFFFF",
   red: "#CD484A",
-  darkRed: "#962F30"
+  darkRed: "#962F30",
 }
