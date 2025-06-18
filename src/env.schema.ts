c08d429455b59ac867f0c0987a0e5ee4df79bbaf
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-06-18 13:43:55 -0300

    remove: removendo a variável SECRET_JWT_KEY das variáveis de ambiente

--- src/env.schema.ts
+++ src/env.schema.ts
@@ -1,5 +1,4 @@
 const SERVER_IP = ""; // Adicionar IP do Servidor
 const SERVER_PORT = ""; // Adicionar a porta do servidor. Default: 3333
-const SECRET_JWT_KEY = ""; // Adicionar chave JWT
 
-export { SERVER_IP, SERVER_PORT, SECRET_JWT_KEY };
+export { SERVER_IP, SERVER_PORT };
